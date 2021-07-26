0f83521933a36fcaa7e2e49f7b98a666fd0700cd
DArmstrong87 <lenzflare@gmail.com>
2021-07-26 17:13:45 -0500

    Converted jewelry style objects to list elements

--- scripts/JewelryStyles.js
+++ scripts/JewelryStyles.js
@@ -12,13 +12,13 @@ export const JewelryStyles = () => {
     let html = "<ul>"
 
     // Use .map() for converting objects to <li> elements
-    const listItemsArray = styles.map(style => {
-        html += `<li>
-        <input type="radio" name="metal" value="${style.style}" />
-        ${style.style}
-        </li>`
+    const listItemsArray = styles.map(
+        style => {
+            html += `<li>
+                    <input type="radio" name="metal" value="${style.style}" />
+                    ${style.style}</li>`
         }
-        )
+    )
 
     // Join all of the strings in the array into a single string
     html += listItemsArray.join("")
